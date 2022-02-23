c79e1cd27111a2f7ec34ace1886aa71d25d402c0
robbejs <43341126+robbejs@users.noreply.github.com>
2022-02-23 11:33:47 +0100

    start backend

---
+++ app/config/db.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  HOST: "188.166.43.149",
+  PORT: "5438",
+  USER: "wolkje",
+  PASSWORD: "aMmmbcdMthiYr6zpdBz35FXsxeEfweR9W2yJUyHnpVZQYPZtzPdWkzvAaSenKprW",
+  DB: "wolkje",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
---
+++ app/routes/campus.routes.js
@@ -0,0 +1,19 @@
+module.exports = app => {
+    const campus = require("../controllers/campus.controller.js");
+
+    var router = require("express").Router();
+
+    router.post("/", campus.create);
+
+    router.get("/", campus.findAll);
+
+    // router.get("/:campus_id", campus.findOne);
+
+    // router.put("/:campus_id", campus.update);
+
+    // router.delete("/:campus_id", campus.delete);
+
+    // router.delete("/", campus.deleteAll);
+
+    app.use('/wolkjes/campus', router);
+}
