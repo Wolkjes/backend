e4da1578335244b8d9898478fb531fd576e8bf36
Othello De Sutter <othello.desutter@uclllabs.be>
2022-02-24 15:15:37 +0100

    added login

--- app/middleware/authJwt.js
+++ app/middleware/authJwt.js
@@ -23,26 +23,26 @@ verifyToken = (req, res, next) => {
   });
 };
 
-isAdmin = (req, res, next) => {
-  User.findByPk(req.userId).then(user => {
-    user.getRoles().then(roles => {
-      for (let i = 0; i < roles.length; i++) {
-        if (roles[i].name === "admin") {
-          next();
-          return;
-        }
-      }
+// isAdmin = (req, res, next) => {
+//   User.findByPk(req.userId).then(user => {
+//     user.getRoles().then(roles => {
+//       for (let i = 0; i < roles.length; i++) {
+//         if (roles[i].name === "admin") {
+//           next();
+//           return;
+//         }
+//       }
 
-      res.status(403).send({
-        message: "Require Admin Role!"
-      });
-      return;
-    });
-  });
-};
+//       res.status(403).send({
+//         message: "Require Admin Role!"
+//       });
+//       return;
+//     });
+//   });
+// };
 
 const authJwt = {
   verifyToken: verifyToken,
-  isAdmin: isAdmin
+  // isAdmin: isAdmin
 };
 module.exports = authJwt;
