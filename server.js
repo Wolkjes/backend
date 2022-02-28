995cce33af91cca10ca97613a0b6147beaa0e751
Othello De Sutter <othello.desutter@uclllabs.be>
2022-02-28 15:03:27 +0100

    minor changes to server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome." });
 });
 
 require("./app/routes/campus.routes")(app);
