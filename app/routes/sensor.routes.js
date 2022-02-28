2d9319af5c3b4e657826a00228fdc484e8efb53b
Othello De Sutter <othello.desutter@uclllabs.be>
2022-02-28 12:39:01 +0100

    added authorization with JWT

--- app/routes/sensor.routes.js
+++ app/routes/sensor.routes.js
@@ -1,17 +1,28 @@
+const { authJwt } = require("../middleware");
 module.exports = app => {
+    app.use(function(req, res, next) {
+        res.header(
+          "Access-Control-Allow-Headers",
+          "x-access-token, Origin, Content-Type, Accept"
+        );
+        next();
+      });
+
     const sensor = require("../controllers/sensor.controller.js");
 
-    var router = require("express").Router();
+    var userRouter = require("express").Router();
+    var adminRouter = require("express").Router();
 
-    router.post("/", sensor.create);
+    // enkel voor admin
+    adminRouter.post("/", sensor.create);
+    adminRouter.put("/:sensor_id", sensor.change);
+    adminRouter.delete("/:sensor_id", sensor.delete);
 
-    router.get("/", sensor.findAll);
+    // ook voor user
+    userRouter.get("/:sensor_id", sensor.findOne);
+    userRouter.get("/", sensor.findAll);
+    userRouter.get("/:sensor_id", sensor.findOne);
 
-    router.get("/:sensor_id", sensor.findOne);
-
-    router.put("/:sensor_id", sensor.change);
-
-    router.delete("/:sensor_id", sensor.delete);
-
-    app.use('/wolkjes/sensor', router);
+    app.use("/wolkjes/sensor", [authJwt.verifyToken, authJwt.isAdmin], userRouter);
+    app.use("/wolkjes/sensor", [authJwt.verifyToken, authJwt.isAdmin], adminRouter);
 }
