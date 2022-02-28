6db30aa4d4bd78bc66aec0e997ac83c810388768
Othello De Sutter <othello.desutter@uclllabs.be>
2022-02-28 11:51:25 +0100

    added middleware and isAdmin function

--- app/middleware/authJwt.js
+++ app/middleware/authJwt.js
@@ -18,31 +18,31 @@ verifyToken = (req, res, next) => {
         message: "Unauthorized!"
       });
     }
-    req.userId = decoded.id;
+    req.email = decoded.email;
     next();
   });
 };
 
-// isAdmin = (req, res, next) => {
-//   User.findByPk(req.userId).then(user => {
-//     user.getRoles().then(roles => {
-//       for (let i = 0; i < roles.length; i++) {
-//         if (roles[i].name === "admin") {
-//           next();
-//           return;
-//         }
-//       }
+isAdmin = (req, res, next) => {
+  User.findOne({
+    where: {
+      email: req.email
+    }
+  }).then(user => {
+    if (user.role === "admin") {
+      next();
+      return;
+    }
 
-//       res.status(403).send({
-//         message: "Require Admin Role!"
-//       });
-//       return;
-//     });
-//   });
-// };
+    res.status(403).send({
+      message: "Admin role required"
+    });
+    return;
+  });
+};
 
 const authJwt = {
   verifyToken: verifyToken,
-  // isAdmin: isAdmin
+  isAdmin: isAdmin
 };
 module.exports = authJwt;
